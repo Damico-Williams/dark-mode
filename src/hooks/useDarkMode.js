7ed538fa3511b8f880bfd994a548ab12dee276d9
damico williams <damicowilliams@gmail.com>
2020-06-11 17:08:23 -0700

    removed key

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,13 +2,15 @@ import useLocalStorage from './useLocalStorage.js';
 import {useEffect} from 'react';
 
 const useDarkMode = (initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage('darkMode', initialValue)
+    const [darkMode, setDarkMode] = useLocalStorage(initialValue)
 
     useEffect(() => {
         darkMode ? document.querySelector('body').classList.add('dark-mode')
-        : document.querySelector('body').classList.remove('dark-mode');
+        : document.querySelector('body').classList.remove('dark-mode')
     }, [darkMode]);
-    return [darkMode, setDarkMode];
+
+    return [darkMode, setDarkMode]
+    
 }
 
 export default useDarkMode;
